9fd6cb53ee5e2ef941dc4a4937811d509c16d8e8
VCGithubCode <vernellclark@hotmail.com>
2024-12-13 10:19:17 +0100

    Clean up main scripts.js file

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,13 +1,13 @@
-/*!
-* Start Bootstrap - Clean Blog v6.0.9 (https://startbootstrap.com/theme/clean-blog)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-clean-blog/blob/master/LICENSE)
-*/
 window.addEventListener('DOMContentLoaded', () => {
     let scrollPos = 0;
     const mainNav = document.getElementById('mainNav');
     const headerHeight = mainNav.clientHeight;
     window.addEventListener('scroll', function() {
+        /**
+         * Calculates the current scroll position from the top of the document.
+         * 
+         * @constant {number} currentTop - The current vertical scroll position of the document body.
+         */
         const currentTop = document.body.getBoundingClientRect().top * -1;
         if ( currentTop < scrollPos) {
             // Scrolling Up
